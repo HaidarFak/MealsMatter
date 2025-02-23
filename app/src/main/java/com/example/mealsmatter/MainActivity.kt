6f3d9d14e0d6b58f68dd5dfbb006fcbd184a67b8
Haidar Fakhruldin <haidarfakhruldin@gmail.com>
2025-02-22 19:02:19 -0500

    Calander Branch

--- app/src/main/java/com/example/mealsmatter/MainActivity.kt
+++ app/src/main/java/com/example/mealsmatter/MainActivity.kt
@@ -1,23 +1,38 @@
 package com.example.mealsmatter
 
 import android.os.Bundle
-import android.widget.CalendarView
-import android.widget.Toast
+import android.os.PersistableBundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.mealsmatter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val calendarView: CalendarView = findViewById(R.id.calendarView)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // Set listener for date change (Built-in CalendarView)
-        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
-            // Show a toast with the selected date
-            val dateString = "$dayOfMonth/${month + 1}/$year"
-            Toast.makeText(this, "Selected Date: $dateString", Toast.LENGTH_SHORT).show()
-        }
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
+
 }
