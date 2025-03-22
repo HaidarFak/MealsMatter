1d84bc4d66883405bb2bd346e9e3173903136ecb
Ahmed Raza <ahmedraz@umich.edu>
2025-03-22 08:03:44 -0400

    import R & showItemDialog added

--- app/src/main/java/com/example/mealsmatter/ui/grocery/GroceryListFragment.kt
+++ app/src/main/java/com/example/mealsmatter/ui/grocery/GroceryListFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mealsmatter.data.GroceryItem
 import com.example.mealsmatter.databinding.FragmentGroceryListBinding
+import com.example.mealsmatter.R
+
 
 class GroceryListFragment : Fragment() {
 
@@ -49,7 +51,7 @@ class GroceryListFragment : Fragment() {
         adapter.updateItems(groceryItems)
 
         binding.fabAddItem.setOnClickListener {
-            // TODO: Add dialog to input new grocery item
+            showAddItemDialog() // TODO: Add dialog to input new grocery item
         }
     }
     private fun showAddItemDialog() {
