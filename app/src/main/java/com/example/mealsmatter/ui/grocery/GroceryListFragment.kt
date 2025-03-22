0cfab73eebda587fdcd93aa3389880bcffcb6e2c
Ahmed Raza <ahmedraz@umich.edu>
2025-03-22 07:48:15 -0400

    added the dialog of grocery item

--- app/src/main/java/com/example/mealsmatter/ui/grocery/GroceryListFragment.kt
+++ app/src/main/java/com/example/mealsmatter/ui/grocery/GroceryListFragment.kt
@@ -5,20 +5,57 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.mealsmatter.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mealsmatter.data.GroceryItem
+import com.example.mealsmatter.databinding.FragmentGroceryListBinding
 
 class GroceryListFragment : Fragment() {
 
+    private var _binding: FragmentGroceryListBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var adapter: GroceryListAdapter
+    private val groceryItems = mutableListOf<GroceryItem>()
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_grocery_list, container, false)
+    ): View {
+        _binding = FragmentGroceryListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Initialize views and setup functionality here
+
+        adapter = GroceryListAdapter(
+            groceryItems,
+            onEditClick = { item -> /* TODO: Show edit dialog */ },
+            onDeleteClick = { item ->
+                groceryItems.remove(item)
+                adapter.updateItems(groceryItems)
+            },
+            onCheckChanged = { item, isChecked ->
+                item.isChecked = isChecked
+            }
+        )
+
+        binding.rvGroceryList.layoutManager = LinearLayoutManager(requireContext())
+        binding.rvGroceryList.adapter = adapter
+
+        // Dummy data for testing
+        groceryItems.add(GroceryItem(name = "Milk", quantity = "2 L"))
+        groceryItems.add(GroceryItem(name = "Eggs", quantity = "1 dozen"))
+        adapter.updateItems(groceryItems)
+
+        binding.fabAddItem.setOnClickListener {
+            // TODO: Add dialog to input new grocery item
+        }
     }
-} 
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+
